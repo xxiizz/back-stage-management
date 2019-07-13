e03bbe5ab767747159511a3989b180b0ff6c61f8
xz <59931713@qq.com>
2019-07-13 21:00:18 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入vue路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const routes = {
+  
+}
+const router = new VueRouter({
+  routes
+})
+
+// 导入饿了么UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
