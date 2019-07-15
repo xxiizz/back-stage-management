e08841f78f4d8a7761b8028ef3aa67828f4361e6
xz <59931713@qq.com>
2019-07-15 15:26:25 +0800

    导入css预编译器,完成reset.css

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入组件
+import login from './components/login.vue'
+
 // 导入vue路由
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-const routes = {
-  
-}
+const routes = [
+  {path:'/login',component:login}
+]
 const router = new VueRouter({
   routes
 })
@@ -15,6 +18,9 @@ const router = new VueRouter({
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
+
+// 导入reset样式
+import './assets/reset.scss'
 
 Vue.config.productionTip = false
 
