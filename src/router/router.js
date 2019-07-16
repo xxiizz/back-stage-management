8d6b8b761d55e5e506826028590e85a3cc550efa
xz <59931713@qq.com>
2019-07-16 09:43:48 +0800

    首页验证登录功能完成

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,28 @@
-import Vue from 'vue'
+import Vue from "vue";
 // 导入组件
-import login from '../views/login.vue'
-import index from '../views/index.vue'
+import login from "../views/login.vue";
+import index from "../views/index.vue";
 
 // 导入vue路由
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
 const routes = [
-  {path:'/login',component:login},
-  {path:'/index',component:index},
-]
+  { path: "/login", component: login },
+  { path: "/index", component: index, meta: { requireLogin: true } }
+];
 const router = new VueRouter({
   routes
-})
-export default router
+});
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireLogin) {
+    if (window.localStorage.getItem("token")) {
+      next();
+    } else {
+      new Vue().$message("请先登录");
+      router.push("/login");
+    }
+  } else {
+    next();
+  }
+});
+export default router;
