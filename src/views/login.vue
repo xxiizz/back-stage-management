12357df964a44f9d321535a48f746d69ad8f3384
xz <59931713@qq.com>
2019-07-16 09:43:20 +0800

    登录页登录功能完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,15 +2,22 @@
   <div class="main">
     <div class="login">
       <h2>用户登录</h2>
-      <el-form :label-position="'top'" label-width="80px" :model="formData" :rules="rules" :status-icon="true">
+      <el-form
+        :label-position="'top'"
+        label-width="80px"
+        :model="formData"
+        :rules="rules"
+        :status-icon="true"
+        ref="loginForm"
+      >
         <el-form-item label="用户名" prop="username">
           <el-input v-model="formData.username" class="elinput"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input v-model="formData.password" type='password' class="elinput"></el-input>
+          <el-input v-model="formData.password" type="password" class="elinput"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" id="btn">登录</el-button>
+          <el-button type="primary" id="btn" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -18,11 +25,12 @@
 </template>
 
 <script>
+import { login } from "../api/api";
 export default {
   name: "login",
   data() {
     return {
-        boo:true,
+      boo: true,
       formData: {
         username: "",
         password: ""
@@ -38,6 +46,30 @@ export default {
         ]
       }
     };
+  },
+  methods: {
+    login() {
+      this.$refs.loginForm.validate(vaild => {
+        if (vaild) {
+          login(this.formData.username, this.formData.password).then(
+            backData => {
+              console.log(backData);
+              if(backData.data.meta.status==400){
+                this.$message.error(backData.data.meta.msg)
+              }
+              else if(backData.data.meta.status==200){
+                this.$message.success(backData.data.meta.msg)
+                window.localStorage.setItem('token',backData.data.data.token)
+                this.$router.push('/index')
+              }
+            }
+          );
+        }
+        else{
+          this.$message.warning('带星号选项为必填项')
+        }
+      });
+    }
   }
 };
 </script>
@@ -69,10 +101,10 @@ export default {
     #btn {
       width: 100%;
     }
-    .is-success{
-        .elinput{
-          outline: 1px solid #67c23a;
-        }
+    .is-success {
+      .elinput {
+        outline: 1px solid #67c23a;
+      }
     }
   }
 }
