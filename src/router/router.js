72bd50a155750eadce0545f5267f9ad678e69c42
xz <59931713@qq.com>
2019-07-16 20:57:14 +0800

    统一的异常页完成

--- src/router/router.js
+++ src/router/router.js
@@ -2,13 +2,16 @@ import Vue from "vue";
 // 导入组件
 import login from "../views/login.vue";
 import index from "../views/index.vue";
+import notfound from "../views/notfound.vue";
 
 // 导入vue路由
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
 const routes = [
   { path: "/login", component: login },
-  { path: "/index", component: index, meta: { requireLogin: true } }
+  { path: "/index", component: index, meta: { requireLogin: true } },
+  { path: "/notfound", component: notfound },
+  { path: "*", redirect: "/notfound" }
 ];
 const router = new VueRouter({
   routes
---
+++ src/views/notfound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="error" @click='toIndex'>
+    <img src="../assets/404.gif" alt />
+  </div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.$message.warning('您好像去火星了,点击飞碟返回地球')
+    },
+    methods: {
+        toIndex(){
+            this.$router.push('/index')
+        }
+    },
+};
+</script>
+
+<style lang="less" scoped>
+.error {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img{
+      width: 100%;
+      height: 100%;
+  }
+}
+</style>
